6bfe25af00f4bc5ea8e78ec0317ed4820eaf0fdf
Misat11 <20199703+Misat11@users.noreply.github.com>
2024-04-24 20:13:36 +0200

    Revert "fix: include 1.8.9" (#3)
    This reverts commit 00b1bfed1b2a61a465a6dc9bfb43a2ee679ac8f9.

--- build.gradle.kts
+++ build.gradle.kts
@@ -112,8 +112,9 @@ val mappingConfig = buildMappingConfig {
         manifest
             .range("1.8.8", "1.20.5") { // change me
                 // exclude 1.16 and 1.10.1, they don't have most mappings and are basically not used at all
+                // exclude 1.8.9, client-only update - no Spigot mappings, no thank you
                 // exclude 1.9.1 and 1.9.3 - no mappings at all
-                exclude("1.16", "1.10.1", "1.9.1", "1.9.3")
+                exclude("1.16", "1.10.1", "1.8.9", "1.9.1", "1.9.3")
 
                 // include only releases, no snapshots
                 includeTypes(Version.Type.RELEASE)

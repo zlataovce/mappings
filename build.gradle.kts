fd8edfe31f00f6654d550ce3462a6746f2ea9703
powercas_gamer <cas@mizule.dev>
2024-04-30 12:11:57 +0200

    1.02.1 and comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -111,10 +111,11 @@ val mappingConfig = buildMappingConfig {
     version(
         manifest
             .range("1.8.8", "1.20.6") { // change me
+                // exclude 1.20.1 and 1.20.6 - hotfix versions                
                 // exclude 1.16 and 1.10.1, they don't have most mappings and are basically not used at all
                 // exclude 1.8.9, client-only update - no Spigot mappings, no thank you
                 // exclude 1.9.1 and 1.9.3 - no mappings at all
-                exclude("1.16", "1.10.1", "1.8.9", "1.9.1", "1.9.3", "1.20.5")
+                exclude("1.16", "1.10.1", "1.8.9", "1.9.1", "1.9.3", "1.20.1", "1.20.5")
 
                 // include only releases, no snapshots
                 includeTypes(Version.Type.RELEASE)

bb0b3ebcb6322db75533f4e4923ad93062e946ff
Matouš Kučera <mk@kcra.me>
2024-10-23 22:29:26 +0200

    feat: 1.21.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ group = "me.kcra.takenaka" // change me
 // format: <oldest version>+<newest version>[-SNAPSHOT]
 // this is included in META-INF/MANIFEST.MF under Implementation-Version
 // be nice to people who use the bundles and don't change the format
-version = "1.8.8+1.21.2" // change me
+version = "1.8.8+1.21.3" // change me
 
 /**
  * A three-way choice of mappings.
@@ -106,12 +106,12 @@ val yarnProvider = YarnMetadataProvider(sharedCacheWorkspace)
 val mappingConfig = buildMappingConfig {
     version(
         manifest
-            .range("1.8.8", "1.21.2") { // change me
-                // exclude 1.20, 1.20.3 and 1.20.5 - hotfixed versions                
+            .range("1.8.8", "1.21.3") { // change me
+                // exclude 1.20, 1.20.3, 1.20.5 and 1.21.2 - hotfixed versions                
                 // exclude 1.16 and 1.10.1, they don't have most mappings and are basically not used at all
                 // exclude 1.8.9, client-only update - no Spigot mappings, no thank you
                 // exclude 1.9.1 and 1.9.3 - no mappings at all
-                exclude("1.16", "1.10.1", "1.8.9", "1.9.1", "1.9.3", "1.20", "1.20.3", "1.20.5")
+                exclude("1.16", "1.10.1", "1.8.9", "1.9.1", "1.9.3", "1.20", "1.20.3", "1.20.5", "1.21.2")
 
                 // include only releases, no snapshots
                 includeTypes(Version.Type.RELEASE)
